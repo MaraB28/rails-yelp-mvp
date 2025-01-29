074cbaec610f7ceb2b7ac8a229fbfb75d0ed8ec1
Mara <bratosinralucamara@yahoo.com>
2025-01-29 11:45:38 +0100

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create!(
+  name: "Xin Gainian",
+  address: "75006 Paris",
+  phone_number: "01 46 33 22 21",
+  category: "chinese"
+)
+
+Restaurant.create!(
+  name: "Marcello",
+  address: "75006 Paris",
+  phone_number: "01 43 26 52 26",
+  category: "italian"
+)
+
+Restaurant.create!(
+  name: "Shin Izakaya",
+  address: "75006 Paris",
+  phone_number: "01 86 04 39 28",
+  category: "japanese"
+)
+
+Restaurant.create!(
+  name: "Le Paris Paris",
+  address: "75006 Paris",
+  phone_number: "01 43 26 50 56",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Label'ge frite Paris 6",
+  address: "75006 Paris",
+  phone_number: "06 69 93 93 63",
+  category: "belgian"
+)
